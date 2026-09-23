fb434eed99f134a770755f1a3e503f20314cbcf6
Cuyqo <saon0410@gmail.com>
2026-09-23 10:16:42 +0900

    feat: blockedId가 실제 존재하는 회원인지 검증 로직 추가

--- src/main/java/com/cotato/nextstation/domain/block/service/command/MemberBlockCommandService.java
+++ src/main/java/com/cotato/nextstation/domain/block/service/command/MemberBlockCommandService.java
@@ -3,18 +3,28 @@ package com.cotato.nextstation.domain.block.service.command;
 import com.cotato.nextstation.domain.block.entity.MemberBlock;
 import com.cotato.nextstation.domain.block.exception.MemberBlockErrorCode;
 import com.cotato.nextstation.domain.block.repository.MemberBlockRepository;
+import com.cotato.nextstation.domain.member.exception.MemberErrorCode;
+import com.cotato.nextstation.domain.member.repository.MemberRepository;
 import com.cotato.nextstation.global.exception.CustomException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@RequiredArgsConstructor
 @Transactional
 public class MemberBlockCommandService {
 
-    public MemberBlockRepository memberBlockRepository;
+    private final MemberBlockRepository memberBlockRepository;
+    private final MemberRepository memberRepository;
 
     public void block(Long blockerId, Long blockedId) {
         if (blockerId.equals(blockedId)) {
             throw new CustomException(MemberBlockErrorCode.SELF_BLOCK_NOT_ALLOWED);
+        }
+        if (!memberRepository.existsById(blockedId)) {
+            throw new CustomException(MemberErrorCode.MEMBER_NOT_FOUND);
         }
         if (memberBlockRepository.existsByBlockerIdAndBlockedId(blockerId, blockedId)) {
             throw new CustomException(MemberBlockErrorCode.ALREADY_BLOCKED);
